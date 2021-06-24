d07e4d7523155d0882d7e49252ff015069350bf3
P3tr0n1us <joel.lafaille@gmail.com>
2021-06-24 10:53:18 +0200

    Updated script to run on sentry node

--- monitoring/oracle-monitoring.sh
+++ monitoring/oracle-monitoring.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+#
+# Upstart monitoring script for oracle missed votes.
+# Run with "setuid root" to be able to restart the feeder service
+#
 
-VALIDATOR=$1
+VALIDATOR=$1 # terravaloper address of your validator
+LCD_HOST=$2 # url to lcd, ex "http://sentry-node:1317" (with quotes)
 echo "Starting oracle-monitoring script..."
 
 # Check every 60 seconds if the missing vote count increases. If more than 2 misses votes in 60 sec, restart the process
 while (true); do
-    missed_votes=$(terracli query oracle miss $VALIDATOR | sed s/\"//g)
+    missed_votes=$(curl -s $LCD_HOST/oracle/voters/$VALIDATOR/miss  | jq ".result" | sed s/\"//g)
+    sleep 60
 
-    sleep 60;
+    last_missed_votes=$(curl -s $LCD_HOST/oracle/voters/$VALIDATOR/miss  | jq ".result" | sed s/\"//g)
+    difference=$(expr $last_missed_votes - $missed_votes + 2)
 
-    last_missed_votes=$(terracli query oracle miss $VALIDATOR | sed s/\"//g)
-    difference=$(expr $last_missed_votes - $missed_votes)
-
-    if [[ $difference -gt 1 ]]
+    if [[ $difference -gt 2 ]]
     then
-	echo 'Missed votes! -> ' $difference
-        restart terrad-price-feeder
-	sleep 60 # wait for stabilisation
+		echo 'Missed votes! -> ' $difference
+		sendmail -t 'email@address.com' << EOF
+Subject: Oracle feeder restarted!
+EOF
+		su -c "restart terra-price-feeder"
+		sleep 60 # wait for stabilisation
     fi
 done;
